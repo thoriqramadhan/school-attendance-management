1d3de3f997ca30751128ed4d4b842c32a20e7b35
thoriqramadhan <respsect.thoriq@gmail.com>
2026-01-07 18:12:09 +0700

    chore: login setup

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,52 +1,43 @@
 "use client"
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
-import { Form, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { Form, FormField, FormItem, FormLabel } from '@/components/ui/form'
 import { useForm } from 'react-hook-form';
 import * as z from "zod";
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
-import { signUpAction } from '../action';
-import { loginSchema } from '../schema';
-import { toast } from 'sonner';
 
-
-export default function LoginPage() {
-    const form = useForm<z.infer<typeof loginSchema>>(
+const registerSchema = z.object({
+    email: z.email(),
+    password: z.string().min(8, { message: 'Password min 8.' })
+})
+export default function RegisterPage() {
+    const form = useForm<z.infer<typeof registerSchema>>(
         {
-            resolver: zodResolver(loginSchema),
+            resolver: zodResolver(registerSchema),
             defaultValues: {
                 email: '',
                 password: ''
             }
         }
     )
-    async function handleSubmit(data: z.infer<typeof loginSchema>) {
-        try {
-            const res = await signUpAction(data)
-            console.log(res);
-
-        } catch (error) {
-            toast.error('Failed login')
-        }
-    }
+    function handleSubmit() { }
     return (
         <Card className='w-full md:w-[500px]'>
-            <CardContent className='w-full  grid grid-cols-1'>
+            <CardContent className='w-full grid grid-cols-1'>
                 <div className="text-xl font-medium">
-                    <p>Login</p>
+                    <p>Register</p>
                     <Form {...form}>
                         <form className='mt-5 space-y-5' onSubmit={form.handleSubmit(handleSubmit)}>
                             <FormField control={form.control} name='email' render={({ field }) => <FormItem >
                                 <FormLabel>Email</FormLabel>
                                 <Input type='email' required {...field} />
-                                <FormMessage />
                             </FormItem>} />
                             <FormField control={form.control} name='password' render={({ field }) => <FormItem >
                                 <FormLabel>Password</FormLabel>
                                 <Input type='password' required {...field} />
-                                <FormMessage />
+
                             </FormItem>} />
                             <Button type='submit' className='w-full'>Submit</Button>
                         </form>
